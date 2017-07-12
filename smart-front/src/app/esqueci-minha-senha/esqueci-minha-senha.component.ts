518ed94787486dbe455d6cdfebb001bc5c26b1db
Sarah Cristina Silva Cruz <sarah.cruz@pe.senai.br>
2017-07-12 15:57:43 -0300

    esqueci minha senha COMPLETO

--- smart-front/src/app/esqueci-minha-senha/esqueci-minha-senha.component.ts
+++ smart-front/src/app/esqueci-minha-senha/esqueci-minha-senha.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule, Validator } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ValidatorsModule, EmailValidators} from 'ngx-validators'
 import {PasswordValidators} from 'ngx-validators'
 
@@ -13,7 +14,54 @@ export class EsqueciMinhaSenhaComponent implements OnInit {
     email: null,
     senha: null
   };
-  constructor() { }
+  emailInput: any;
+  emailNaoCadastrado: boolean = false;
+  aparecerMsgEmailInvalido: boolean = false;
+  emailIgual: boolean = false;
+  inputVazio: boolean = false;
+  aparecerMsg: boolean = false;
+  usuarios: any[] = [
+    {email: 'admin@isi.com', senha: 'admin'},
+    {email: 'admin2@isi.com', senha: 'admin2'},
+    {email: 'admin3@isi.com', senha: 'admin3'}
+  ];
+
+  constructor(private router: Router) { }
+
+  testar(input){
+    this.emailNaoCadastrado = false;
+    this.inputVazio = false;
+    this.aparecerMsg = false;
+    this.aparecerMsgEmailInvalido = true;
+    var i;
+    var f = this.usuarios.length;
+
+    for(i = 0; i < f ; i ++ ){
+      if(input == this.usuarios[i].email){
+          this.emailIgual = true;
+          alert("Mandamos o seu email!")
+          this.router.navigate(['/rc']);
+          return true;
+      } else {
+        if(i == (f-1)){
+          if((input == null)||(input.length == 0)){
+            this.inputVazio = true;
+            return false;
+          }else {
+            this.aparecerMsg = true;
+            return false;
+          }
+        }
+      }
+    }
+  }
+  limpar(){
+
+  }
+
+  prosseguir(){
+    this.testar(this.emailInput);
+  }
 
   ngOnInit() {
   }
