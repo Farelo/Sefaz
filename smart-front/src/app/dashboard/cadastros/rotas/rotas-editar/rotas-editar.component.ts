b2021c6016d0bdfc2f0a76814df1e48091f478d6
Emanoel Carlos <emanoel.carlos@hotmail.com>
2018-07-05 14:45:31 -0300

    ajuste no mapa directions em editar rota

--- smart-front/src/app/dashboard/cadastros/rotas/rotas-editar/rotas-editar.component.ts
+++ smart-front/src/app/dashboard/cadastros/rotas/rotas-editar/rotas-editar.component.ts
@@ -6,9 +6,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { DirectionsRenderer } from '@ngui/map';
 import { ToastService, RoutesService, PlantsService, SuppliersService, PackingService } from '../../../../servicos/index.service';
 import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
-import {NgbTimeStruct} from '@ng-bootstrap/ng-bootstrap';
-import { Subscription } from 'rxjs/Rx'; 
-declare var $:any;
+import { NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs/Rx';
+declare var $: any;
 
 @Component({
   selector: 'app-rotas-editar',
@@ -17,26 +17,26 @@ declare var $:any;
 })
 export class RotasEditarComponent implements OnInit {
   @ViewChild(DirectionsRenderer) directionsRendererDirective: DirectionsRenderer;
-  public time_min: NgbTimeStruct = {hour: 0, minute: 0, second: 0};
-  public time_max: NgbTimeStruct = {hour: 0, minute: 0, second: 0};
+  public time_min: NgbTimeStruct = { hour: 0, minute: 0, second: 0 };
+  public time_max: NgbTimeStruct = { hour: 0, minute: 0, second: 0 };
   public time_delay: NgbTimeStruct = { hour: 0, minute: 0, second: 0 };
 
   public directionsRenderer: google.maps.DirectionsRenderer;
   public directionsResult: google.maps.DirectionsResult;
   public direction: any = {
-    origin: '',
-    destination: '',
+    origin: new google.maps.LatLng(0, 0),
+    destination: new google.maps.LatLng(0, 0),
     travelMode: 'DRIVING'
   };
   public inscricao: Subscription;
   public autocomplete: any;
   public address: any = {};
-  public center: any;
+  public center: any = new google.maps.LatLng(0, 0);
   public pos: any;
   public directions = false;
   public plant_factory: any = "";
   public route: FormGroup;
-  public suppliers =  [];
+  public suppliers = [];
   public plants = [];
   public packings = [];
 
@@ -51,11 +51,11 @@ export class RotasEditarComponent implements OnInit {
     private ref: ChangeDetectorRef,
     private toastService: ToastService,
     private fb: FormBuilder,
-    ) {
+  ) {
 
     this.route = this.fb.group({
-      supplier:['', [Validators.required]],
-      project:[''],
+      supplier: ['', [Validators.required]],
+      project: [''],
       plant_factory: ['', [Validators.required]],
       plant_supplier: ['', [Validators.required]],
       packing_code: ['', [Validators.required]],
@@ -63,8 +63,8 @@ export class RotasEditarComponent implements OnInit {
       _id: ['', [Validators.required]],
       __v: ['', [Validators.required]],
       time: this.fb.group({
-        max:  ['', [Validators.required]],
-        min:  ['', [Validators.required]],
+        max: ['', [Validators.required]],
+        min: ['', [Validators.required]],
         delay: ['', [Validators.required]]
       }),
       location: this.fb.group({
@@ -82,6 +82,8 @@ export class RotasEditarComponent implements OnInit {
       })
     });
 
+    console.log('[constructor] center: ' + JSON.stringify(this.center));
+
     this.retrieveRoute();
 
   }
@@ -89,13 +91,13 @@ export class RotasEditarComponent implements OnInit {
 
   onSubmit({ value, valid }: { value: any, valid: boolean }): void {
     //fazer a tranformação para segundo, somar e dar o resultado.
-    let partial_min = this.time_min.hour * 1000 * 60 * 60 * 24 ;
-    partial_min = partial_min + this.time_min.minute * 1000 * 60 * 60  ;
-    partial_min = partial_min + this.time_min.second * 1000 * 60 ;
+    let partial_min = this.time_min.hour * 1000 * 60 * 60 * 24;
+    partial_min = partial_min + this.time_min.minute * 1000 * 60 * 60;
+    partial_min = partial_min + this.time_min.second * 1000 * 60;
 
-    let partial_max = this.time_max.hour * 1000 * 60 * 60 * 24 ;
-    partial_max = partial_max + this.time_max.minute * 1000 * 60 * 60  ;
-    partial_max = partial_max + this.time_max.second * 1000 * 60 ;
+    let partial_max = this.time_max.hour * 1000 * 60 * 60 * 24;
+    partial_max = partial_max + this.time_max.minute * 1000 * 60 * 60;
+    partial_max = partial_max + this.time_max.second * 1000 * 60;
 
     let partial_delay = this.time_delay.hour * 1000 * 60 * 60 * 24;
     partial_delay = partial_delay + this.time_delay.minute * 1000 * 60 * 60;
@@ -107,10 +109,10 @@ export class RotasEditarComponent implements OnInit {
     value.time.min = partial_min;
     value.time.to_be_late = partial_delay;
 
-    if(this.route.valid){
+    if (this.route.valid) {
 
       this.RoutesService
-        .updateRoute(value._id,value)
+        .updateRoute(value._id, value)
         .subscribe(result => {
           this.toastService.edit('/rc/cadastros/rotas', 'Rota')
         }, err => this.toastService.error(err));
@@ -129,7 +131,7 @@ export class RotasEditarComponent implements OnInit {
 
       console.log('[directionsChanged] this.directionsResult: ' + JSON.stringify(this.directionsResult));
       console.log('[directionsChanged] this.direction: ' + JSON.stringify(this.direction));
-      
+
     } else {
       this.directions = false;
     }
@@ -151,7 +153,7 @@ export class RotasEditarComponent implements OnInit {
 
   }
 
-  retrieveRoute(){
+  retrieveRoute() {
     this.inscricao = this.routeActive.params.subscribe(
       (params: any) => {
         let id = params['id'];
@@ -185,13 +187,16 @@ export class RotasEditarComponent implements OnInit {
           (<FormGroup>this.route)
             .patchValue(result.data, { onlySelf: true });
 
-
           this.direction.origin = new google.maps.LatLng(result.data.plant_factory.lat, result.data.plant_factory.lng);
           this.direction.destination = new google.maps.LatLng(result.data.plant_supplier.lat, result.data.plant_supplier.lng);
+
+          this.center = this.direction.origin;
+
           this.directionsRendererDirective['initialized$'].subscribe(directionsRenderer => {
             this.directionsRenderer = directionsRenderer;
           });
 
+          console.log('[retrieveRoute] this.center: ' + JSON.stringify(this.center));
           console.log('[retrieveRoute] result: ' + JSON.stringify(result));
           console.log('[retrieveRoute] this.direction: ' + JSON.stringify(this.direction));
         });
