9f9bc159832f541ce97620e21e60ac90c13f911a
Emanoel Carlos <emanoel.carlos@hotmail.com>
2018-07-25 13:12:38 -0300

    bpline atualizado e filtro corrigido para equipamento

--- smart-front/src/app/dashboard/gc16/gc16.component.ts
+++ smart-front/src/app/dashboard/gc16/gc16.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { GC16Service } from '../../servicos/index.service';
+import { GC16Service, AuthenticationService, PackingService, InventoryLogisticService, InventoryService } from '../../servicos/index.service';
 import { Pagination } from '../../shared/models/pagination';
 import { ModalDeleteComponent } from '../../shared/modal-delete/modal-delete.component';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
@@ -10,23 +10,70 @@ import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./gc16.component.css']
 })
 export class Gc16Component implements OnInit {
-  public data: Pagination = new Pagination({meta: {page : 1}});
-  public search  = "";
-  constructor(
-    private GC16Service: GC16Service,
-    private modalService: NgbModal
-  ) { }
 
-  searchEvent(): void{
-      this.loadGC16();
+  public logged_user: any;
+  public data: Pagination = new Pagination({meta: {page : 1}});
+  public equipamentSearch = "";
+  public packings: any[];
+ 
+  constructor(
+    private packingService: PackingService,
+    private inventoryLogisticService: InventoryLogisticService,
+    private inventoryService: InventoryService,
+    private GC16Service: GC16Service,
+    private modalService: NgbModal,
+    private auth: AuthenticationService) { 
+
+    let user = this.auth.currentUser();
+    let current_user = this.auth.currentUser();
+    this.logged_user = (user.supplier ? user.supplier._id : (
+      user.official_supplier ? user.official_supplier : (
+        user.logistic ? user.logistic.suppliers : (
+          user.official_logistic ? user.official_logistic.suppliers : undefined)))); //works fine
   }
+
+  ngOnInit() {
+    this.loadPackings();
+    this.loadGC16();
+  }
+
 
   loadGC16() {
     this.GC16Service
-      .getGC16sPagination(10, this.data.meta.page ,this.search)
+      .getGC16sPagination(10, this.data.meta.page, this.equipamentSearch)
       .subscribe(result => {
          this.data = result;
+        console.log('result: ' + JSON.stringify(result));
        },err => {  console.log(err)});
+  }
+
+  loadPackings() {
+    console.log('loadPackings');
+
+    if (this.logged_user instanceof Array) {
+      this.packingService.getPackingsDistinctsByLogistic(this.logged_user).subscribe(result => {
+        this.packings = result.data;
+        console.log('this.packings: ' + JSON.stringify(this.packings));
+      }, err => { console.log(err) });
+
+    } else if (this.logged_user) {
+      this.packingService.getPackingsDistinctsBySupplier(this.logged_user).subscribe(result => {
+        this.packings = result.data;
+        console.log('this.packings: ' + JSON.stringify(this.packings));
+      }, err => { console.log(err) });
+
+    } else {
+      this.packingService.getPackingsDistincts().subscribe(result => {
+        this.packings = result.data;
+        console.log('this.packings: ' + JSON.stringify(this.packings));
+      }, err => { console.log(err) });
+    }
+  }
+
+  equipamentChanged(){
+    console.log('equipamentChanged');
+    if (this.equipamentSearch == null) this.equipamentSearch = "";
+    this.loadGC16();
   }
 
   //refazer isso daqui, depende de muitas coisas para ser realizada a alteração
@@ -42,10 +89,6 @@ export class Gc16Component implements OnInit {
 
   pageChanged(page: any): void{
     this.data.meta.page = page;
-    this.loadGC16();
-  }
-
-  ngOnInit() {
     this.loadGC16();
   }
 
