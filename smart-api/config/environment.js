e32d0b9bb8ec37bcc3a1e0f72be85faeb410c009
Emanoel Carlos <emanoel.carlos@hotmail.com>
2018-08-13 17:56:37 -0300

    merging

--- smart-api/config/environment.js
+++ smart-api/config/environment.js
@@ -11,13 +11,8 @@ const environment = {
   development: {
     url: 'localhost',
     port: '8984',
-<<<<<<< HEAD
-    //database: 'testeiterno', // base de dados utilizada
-    database: 'reciclatest', // base de dados utilizada
-=======
     database: 'cebrace', // base de dados utilizada
     //database: 'reciclatest', // base de dados utilizada
->>>>>>> feature/novoFront
     urldatabase: 'localhost', // url de acesso a base de dados
     time: '50', // tempo da execução do job em segundos 1-59
     secret: 'S3cr3t', // chave privada da tokenização
