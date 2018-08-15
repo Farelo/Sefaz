55a7feba1c66008867dee1e4048d2015cf0719cd
Emanoel Carlos <emanoel.carlos@hotmail.com>
2018-08-15 13:36:53 -0300

    correção equipamento geral

--- smart-front/src/app/shared/pipes/round.ts
+++ smart-front/src/app/shared/pipes/round.ts
@@ -3,6 +3,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name: 'round'})
 export class RoundPipe {
   transform (input:number) {
+    if(input ==undefined) return '-';
     return Math.floor(input);
   }
 }
