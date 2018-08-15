a9eff3f6c372ff24c2c8eac955ac008c647a610c
Emanoel Carlos <emanoel.carlos@hotmail.com>
2018-08-15 18:40:25 -0300

    modal local incorreto corrigido

--- smart-front/src/app/shared/modal-alerta/alerta-bateria-baixa/alerta-bateria-baixa.component.ts
+++ smart-front/src/app/shared/modal-alerta/alerta-bateria-baixa/alerta-bateria-baixa.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LayerModalComponent } from '../../modal-packing/layer.component';
+import { Pagination } from '../../models/pagination';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { InventoryService, InventoryLogisticService } from '../../../servicos/index.service';
+import { constants } from '../../../../environments/constants';
 
 @Component({
   selector: 'app-alerta-bateria-baixa',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertaBateriaBaixaComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  alerta;
+  public historic: Pagination = new Pagination({ meta: { page: 1 } });
+
+  constructor(
+    public activeAlerta: NgbActiveModal,
+    private inventoryService: InventoryService,
+    private inventoryLogisticService: InventoryLogisticService,
+    private modalService: NgbModal,
+  ) { }
 
   ngOnInit() {
+    console.log(this.alerta); 
   }
+
+  visualizeOnMap() {
+    //console.log('open map');
+    //console.log('alerta: ' + JSON.stringify(this.alerta));
+    //console.log('[alerta-component] alerta: ' + JSON.stringify(this.alerta.data.packing));
+
+    //http://localhost:8984/api/inventory/general/packings/10/1?code=5039991
+
+    this.inventoryService
+      .getInventoryGeneralPackings(10, 1, this.alerta.data.packing.code_tag, '')
+      .subscribe(
+        result => {
+          let actualPackage = result.data;
+          //console.log('actualPackage: ' + JSON.stringify(actualPackage[0]));
+
+          this.activeAlerta.dismiss('open map');
+          const modalRef = this.modalService.open(LayerModalComponent, {
+            backdrop: 'static',
+            size: 'lg',
+            windowClass: 'modal-xl',
+          });
+          actualPackage[0].alertCode = this.alerta.data.status;
+          modalRef.componentInstance.packing = actualPackage[0];
+        },
+        err => {
+          console.log(err);
+        },
+    );
+  }
+
+  generalInventoryEquipament() { }
 
 }
