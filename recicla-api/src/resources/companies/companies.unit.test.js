8bf89ea475119a174c2580fefc7c50605359f3a1
giorgiosaints <giorgiosaints@gmail.com>
2018-10-09 18:40:40 -0300

    RC-13 #time 30m #comment Teste unitário de validação do _id adicionado

--- recicla-api/src/resources/companies/companies.unit.test.js
+++ recicla-api/src/resources/companies/companies.unit.test.js
@@ -1,3 +1,13 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 // const config = require('config')
 // const { Company } = require('./companies.model')
+
+describe('Company unit test', () => {
+    describe('validate_object_id', () => {
+        it('should validate a object_id', () => {
+            const obj_id = mongoose.Types.ObjectId()
+            const result = mongoose.Types.ObjectId.isValid(obj_id)
+            expect(result).toBe(true)
+        })
+    })
+})
