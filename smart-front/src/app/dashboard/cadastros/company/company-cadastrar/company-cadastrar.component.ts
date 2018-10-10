2c39a67083c85e9df6a0f1ee48a6a4e075c032cf
giorgiosaints <giorgiosaints@gmail.com>
2018-10-10 19:41:46 -0300

    RC-13 #time 2h #comment Listagem, cadastro e remoção do resource Company completo

--- smart-front/src/app/dashboard/cadastros/company/company-cadastrar/company-cadastrar.component.ts
+++ smart-front/src/app/dashboard/cadastros/company/company-cadastrar/company-cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, AbstractControl } from '@angular/forms';
+import { CompaniesService } from '../../../../servicos/companies.service';
+import { ToastService } from '../../../../servicos/index.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-company-cadastrar',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyCadastrarComponent implements OnInit {
 
-  constructor() { }
+  public newCompany: FormGroup;
+  public typesOnSelect: any = [];
+
+  constructor(
+    protected companiesService: CompaniesService,
+    protected toastService: ToastService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.formProfile();
+    this.fillSelectType();
+  }
+
+  fillSelectType() {
+    this.typesOnSelect = [
+      { label: "Owner", name: "owner" },
+      { label: "Cliente", name: "client" }];
+  }
+
+  formProfile() {
+    this.newCompany = this.fb.group({
+      name: ['', [Validators.required]],
+      phone: '',
+      address: this.fb.group({
+        city: '',
+        street: '',
+        cep: '',
+        uf: ''
+      }),
+      type: ''
+    });
+  }
+
+  onSubmit({ value, valid }: { value: any, valid: boolean }): void {
+    if (valid) {
+      value.type = value.type.name;
+      console.log(value);
+
+      this.companiesService
+        .createCompany(value)
+        .subscribe(result => { this.router.navigate(['/rc/cadastros/company']); this.toastService.successModal('Empresa criada!') });
+    }
   }
 
 }
