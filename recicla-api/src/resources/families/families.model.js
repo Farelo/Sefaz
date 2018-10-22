9666c3641b708c39b7113f3233da431fba3e41a9
giorgiosaints <giorgiosaints@gmail.com>
2018-10-22 08:11:56 -0300

    Removendo arrays de dentro das collectiomns Company e Family

--- recicla-api/src/resources/families/families.model.js
+++ recicla-api/src/resources/families/families.model.js
@@ -7,10 +7,6 @@ const familySchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    packings: [{
-        type: mongoose.Schema.ObjectId,
-        ref: 'Packing'
-    }],
     routes: [{
         type: mongoose.Schema.ObjectId,
         ref: 'Route'
