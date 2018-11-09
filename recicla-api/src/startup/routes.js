a57affbd9fc8ca875f6c4d9fe2c15d31ca865026
Miguel Lima <miguel91_it@hotmail.com>
2018-11-09 09:20:46 -0200

    commit para pull do release/sprint-5

--- recicla-api/src/startup/routes.js
+++ recicla-api/src/startup/routes.js
@@ -9,6 +9,7 @@ const control_points = require('../resources/control_points/control_points.routi
 const types = require('../resources/types/types.routing')
 const routes = require('../resources/routes/routes.routing')
 const departments = require('../resources/departments/departments.routing')
+const device_data = require('../resources/device_data/device_data.routing')
 const projects = require('../resources/projects/projects.routing')
 const reports = require('../resources/reports/reports.routing')
 const error = require('../middlewares/error_handler.middleware')
@@ -40,6 +41,7 @@ module.exports = (app) => {
     app.use('/api/departments', departments)
     app.use('/api/projects', projects)
     app.use('/api/reports', reports)
+    app.use('/api/device_data', device_data)
 
     // Middlewares functions
     app.use(error)
--- recicla-api/src/startup/startup_user.js
+++ recicla-api/src/startup/startup_user.js
@@ -3,6 +3,10 @@ const { User } = require('../resources/users/users.model')
 const { Company } = require('../resources/companies/companies.model')
 const config = require('config')
 
+const dm_temp_test = require('../services/loka/dm.temp.testes')
+const job = require('../Jobs/loka/dm.job')
+const initialize_data = require('./temp-initialize.script')
+
 const startupUser = async () => {
     try {
         const companies = await Company.find()
@@ -17,6 +21,18 @@ const startupUser = async () => {
             debug('Startup user with success.')
         } else {
             debug('Company and User already created.')
+
+            // await initialize_data()
+
+            // await dm_temp_test.test_login()
+            // await dm_temp_test.test_messages()
+            // await dm_temp_test.test_positions()
+            // dm_temp_test.test_deviceById()
+            // dm_temp_test.test_devices_list()
+            // dm_temp_test.test_confirmDevice()
+            // dm_temp_test.test_getDeviceDataFromMidd()
+            
+            await job()
         }
     } catch (error) {
         debug('Something failed when startup a user.')
