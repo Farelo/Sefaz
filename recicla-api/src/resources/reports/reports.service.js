1dde76ac6019a84673cd175b0db8d5b526723174
giorgiosaints <giorgiosaints@gmail.com>
2018-11-13 14:50:25 -0300

    Mudando de dias para horas o tempo de viagem

--- recicla-api/src/resources/reports/reports.service.js
+++ recicla-api/src/resources/reports/reports.service.js
@@ -10,3 +10,13 @@ exports.general = async () => {
         throw new Error(error)
     }
 }
+
+exports.general_inventory = async () => {
+    try {
+        const data = await reports_repository.general_inventory_report()
+        return data
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
