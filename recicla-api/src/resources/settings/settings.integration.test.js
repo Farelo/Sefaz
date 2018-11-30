3cd2218c7e9c5713266fb9d3475e1c5e580418a2
Luiz Antônio de Pontes Macedo <luiz.macedo@pe.senai.br>
2018-11-30 16:29:13 -0300

    Atualizando os testes de integração do resource settings

--- recicla-api/src/resources/settings/settings.integration.test.js
+++ recicla-api/src/resources/settings/settings.integration.test.js
@@ -31,7 +31,7 @@ describe('api/settings', () => {
             enable_gc16: true,
             battery_level_limit: 20,
             accuracy_limit: 100,
-            job_schedule_time: 50,
+            job_schedule_time_in_sec: 50,
             range_radius: 3000,
             clean_historic_moviments_time: 5,
             no_signal_limit_in_days: 2,
@@ -51,7 +51,7 @@ describe('api/settings', () => {
     })
 
     describe('AUTH MIDDLEWARE', () => {
-        // jest.setTimeout(30000)
+        jest.setTimeout(30000)
         
         describe('Validate token by GET method without id', () => {
             const exec = () => {
@@ -194,10 +194,6 @@ describe('api/settings', () => {
     })
 
     describe('PATCH: /api/settings/:id', () => {
-        beforeAll(async () => {
-            setting_body.enable_gc16 = false
-            setting_body.battery_level_limit = 15
-        })
         
         const exec = () => {
             return request(server)
@@ -208,7 +204,7 @@ describe('api/settings', () => {
 
         it('should return 404 if invalid id is passed', async () => {
             const res = await request(server)
-                .get(`/api/settings/1`)
+                .patch(`/api/settings/1`)
                 .set('Authorization', token)
 
             expect(res.status).toBe(404)
@@ -216,7 +212,7 @@ describe('api/settings', () => {
 
         it('should return 404 if id is not provied', async () => {
             const res = await request(server)
-                .get(`/api/settings/`)
+                .patch(`/api/settings/`)
                 .set('Authorization', token)
 
             expect(res.status).toBe(404)
@@ -245,7 +241,7 @@ describe('api/settings', () => {
             const res = await exec()
             const body = _.omit(res.body, ["__v", "created_at", "update_at"])
 
-            expect(res.status).toBe(400)
+            expect(res.status).toBe(200)
             expect(body).toEqual(setting)
         })
 
@@ -270,6 +266,54 @@ describe('api/settings', () => {
             expect(res.status).toBe(400)
             expect(res.body).toEqual([
                 "\"test\" is not allowed"
+            ])
+        })
+
+        it('should return 400 if attributes has below of boundaries', async () => {
+            setting_body.battery_level_limit = -20,
+            setting_body.accuracy_limit = -100,
+            setting_body.job_schedule_time_in_sec = -50,
+            setting_body.range_radius = -3000,
+            setting_body.clean_historic_moviments_time = -5,
+            setting_body.no_signal_limit_in_days = -2,
+            setting_body.missing_sinal_limit_in_days = -1
+
+            const res = await exec()
+
+            expect(res.status).toBe(400)
+            expect(res.body).toEqual([
+                "\"battery_level_limit\" must be larger than or equal to 0",
+                "\"accuracy_limit\" must be larger than or equal to 0",
+                "\"job_schedule_time_in_sec\" must be larger than or equal to 0",
+                "\"range_radius\" must be larger than or equal to 0",
+                "\"clean_historic_moviments_time\" must be larger than or equal to 0",
+                "\"no_signal_limit_in_days\" must be larger than or equal to 0",
+                "\"missing_sinal_limit_in_days\" must be larger than or equal to 0"
+            ])
+        })
+
+        it('should return 400 if the attribute types diferent than expected', async () => {
+            setting_body.enable_gc16 = 11
+            setting_body.battery_level_limit = 'asd',
+            setting_body.accuracy_limit = 'asd',
+            setting_body.job_schedule_time_in_sec = 'asd',
+            setting_body.range_radius = 'asd',
+            setting_body.clean_historic_moviments_time = 'asd',
+            setting_body.no_signal_limit_in_days = 'asd',
+            setting_body.missing_sinal_limit_in_days = 'asd'
+
+            const res = await exec()
+
+            expect(res.status).toBe(400)
+            expect(res.body).toEqual([
+                "\"enable_gc16\" must be a boolean",
+                "\"battery_level_limit\" must be a number",
+                "\"accuracy_limit\" must be a number",
+                "\"job_schedule_time_in_sec\" must be a number",
+                "\"range_radius\" must be a number",
+                "\"clean_historic_moviments_time\" must be a number",
+                "\"no_signal_limit_in_days\" must be a number",
+                "\"missing_sinal_limit_in_days\" must be a number"
             ])
         })
     })
