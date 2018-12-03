3c4b155e93d3c4e172bed9830049f2f622091cac
Miguel Lima <miguel91_it@hotmail.com>
2018-12-03 18:33:20 -0200

    melhorias no controle do job loka

--- recicla-api/src/jobs/loka/main.script.js
+++ recicla-api/src/jobs/loka/main.script.js
@@ -4,32 +4,37 @@ const debug = require('debug')('cron-job:loka-dm-main')
 
 //variável sinalizadora para o pŕoximo job de fato executar, essencial 
 let next = true
+let count = 1
 
 module.exports = () => {
-    cron.schedule(`* */1 * * * *`, async () => {
+    cron.schedule(`* */3 * * * *`, async () => {
         // debug('job cron fora')
 
         if (next) {
 
-            // debug('job cron dentro ')
-
             next = false
 
-            // debug('next: ',next)
+            debug(`Job ${count} iniciado`)
+
+            let timeStart = new Date()
 
             //sincrono -> se tiver algo a rodar necessariamente após o termino da função exec(), que vai ser a chamada ao job
-            await exec().then(res => {next = res; debug('job encerrado')}).catch(error => debug('job com erro: ', error))
+            await exec().then(job_result => debug(job_result)).catch(error => debug(error))
+
+            debug(`Job ${count} encerrado com inicio em  ${timeStart} e finalizado em  ${new Date()}`)
+
+            count++
+
+            next = true
 
             //assincrono
             // exec().then(res => {next = res; debug('job encerrado')}).catch(error => debug('job com erro: ', error))
             
-            next = next == true ? next : false
-
-            // debug('next: ',next)
+            // next = next == true ? next : false
         }
     })
 }
-let count = 0
+
 //TODO: analisar quando a promise eh rejeitada
 //ver o que acontece no cron (ver se tem relação com o travamento repentino do job)
 const exec = () => {
@@ -37,20 +42,14 @@ const exec = () => {
     return new Promise(async (resolve, reject) => {
 
         try {
-            let timeStart = new Date()
+            let job_result = await dm_job()
 
-            await dm_job()
-
-            count++
-
-            debug('Job ' + count + ' encerrado com inicio em ', timeStart, ' e finalizado em ', new Date())
-            
-            resolve(true)
+            resolve(job_result)
 
         } catch (error) {
             
             reject(error)
-
+            
         }
     })
 }
