cac3f0d64b9e4da372627c810238aee3d8ed7d5f
Miguel Lima <miguel91_it@hotmail.com>
2018-12-06 11:55:44 -0200

    commit do device_data.routing para fazer merge

--- recicla-api/src/resources/device_data/device_data.routing.js
+++ recicla-api/src/resources/device_data/device_data.routing.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const device_data_controller = require('./device_data.controller')
-// const auth = require('../../security/auth.middleware')
-// const authz = require('../../security/authz.middleware')
+const auth = require('../../security/auth.middleware')
+const authz = require('../../security/authz.middleware')
 
-router.get('/:device_id', device_data_controller.all)
+router.get('/:device_id', [auth, authz], device_data_controller.all)
+
+//serginho comentou o get abaixo porque 
 // router.get('/:device_id', auth, alerts_controller.all)
 
 module.exports = router
