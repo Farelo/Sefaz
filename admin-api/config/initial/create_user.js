02dc759849891fcf715074225c399468d9f41ac6
Miguel Lima <miguel91_it@hotmail.com>
2018-12-11 16:00:25 -0200

    Exclusão das pastas admin-front e admin-api do projeto

--- admin-api/config/initial/create_user.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const constants          = require('../../api/helpers/utils/constants');
-const schemas            = require('../database/require_schemas')
-
-schemas.user().create(constants.system_user)
