6875efe75208e8ee91b397bcbb153dbcf8871cb2
Miguel Lima <miguel91_it@hotmail.com>
2018-12-11 16:05:57 -0200

    Exclusão da pasta smart-api pq foi desmembrada em 3 novos back-ends

--- smart-api/api/helpers/responses/general/successHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const HTTPStatus = require("http-status");
-
-function onSuccess(res, refresh_token, data) {
-  res.status(HTTPStatus.OK).json({ jsonapi: { "version": "1.0" }, "refresh_token": refresh_token, status: HTTPStatus.OK , data: data });
-}
-
-module.exports = onSuccess;
--- smart-api/job/common/alerts_type.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MISSING: 1,
-  INCORRECT_LOCAL: 2,
-  BATTERY: 3,
-  LATE: 4,
-  PERMANENCE: 5,
-};
