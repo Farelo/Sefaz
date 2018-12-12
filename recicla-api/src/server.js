5f056b90a95432d0502b1eb610ed15a85a224bb4
Miguel Lima <miguel91_it@hotmail.com>
2018-12-12 00:58:49 -0200

    remoção das chamadas ao job loka, pq agora virou um projeto em si

--- recicla-api/src/server.js
+++ recicla-api/src/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const logger = require('./config/winston.config')
 const config = require('config')
 require('express-async-errors')
-// const dm_main_script = require('./jobs/loka/main.script')
 
 const app = express()
 
@@ -11,8 +10,6 @@ if (config.get('swagger.enabled')) require('./startup/swagger')(app)
 require('./startup/routes')(app)
 require('./startup/db')()
 if (config.get('company.enabled')) require('./startup/startup_user')()
-
-// dm_main_script()
 
 const server = app.listen(config.get('server.port'), () => {
     logger.info(`Server is running on port: ${config.get('server.port')}`)
