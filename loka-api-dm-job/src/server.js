d319b17e8985af5b3b815e68f6bba4d0012019cd
Miguel Lima <miguel91_it@hotmail.com>
2018-12-15 03:56:53 -0200

    incluindo a inicialização do job-loka ou da carga-inicial

--- loka-api-dm-job/src/server.js
+++ loka-api-dm-job/src/server.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const logger = require('./config/winston.config')
 const config = require('config')
-const main = require('./scripts/main.script')
 const app = express()
+const start_job_loka = require('./scripts/main.script')
+const start_initial_load = require('./scripts/initial-load/initial-load')
 
 require('./startup/logger')(app)
 require('./startup/db')()
 
-main()
+//TODO: colocar e testar pra quando o ENV=test nao fazer nenhum dos dois
+if(process.env.NODE_ENV != 'test'){
+    if (process.env.INITIAL_LOAD)
+        start_initial_load()
+    else
+        start_job_loka()
+}
 
 const server = app.listen(config.get('server.port'), () => {
     logger.info(`Server is running on port: ${config.get('server.port')}`)
