8c08830490f8b03a5a8b029a416373aeecae244c
David Wilson de Farias Santos <david.santos@pe.senai.br>
2018-12-19 15:00:13 -0200

    alteração na rota do front

--- relog-frontend-web/src/app/app.module.ts
+++ relog-frontend-web/src/app/app.module.ts
@@ -1,56 +1,66 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HttpModule } from '@angular/http';
-import { NgbModule, NgbModal, NgbActiveModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
-import { NgProgressBrowserXhr, NgProgressModule , NgProgressInterceptor} from 'ngx-progressbar';
-import { NgxPaginationModule} from 'ngx-pagination';
-import { JWBootstrapSwitchModule } from 'jw-bootstrap-switch-ng2';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import * as $ from 'jquery';
-import { AppComponent } from './app.component';
-import { ModalRastComponent } from './shared/modal-rast/modal-rast.component';
-import { ModalUserComponent } from './shared/modal-user/modal-user.component';
-import { ModalSupplierRegisterComponent } from './shared/modal-user/modal-register-supplier/modal-register-supplier.component';
-import { ModalLogisticRegisterComponent } from './shared/modal-user/modal-register-logistic/modal-register-logistic.component';
-import { ModalStaffRegisterComponent } from './shared/modal-user/modal-register-staff/modal-register-staff.component';
-import { ModalSupplierEditarComponent } from './shared/modal-user/modal-editar-supplier/modal-editar-supplier.component';
-import { ModalLogisticEditarComponent } from './shared/modal-user/modal-editar-logistic/modal-editar-logistic.component';
-import { ModalCurrentEditarComponent } from './shared/modal-current-edit/modal-editar-current.component';
-import { ModalStaffEditarComponent } from './shared/modal-user/modal-editar-staff/modal-editar-staff.component';
-import { AbscenseModalComponent } from './shared/modal-packing-absence/abscense.component';
-import { ModalSettings } from './shared/modal-settings/modal-settings.component';
-import { ModalInvComponent } from './shared/modal-inv/modal-inv.component';
-import { ApplicationPipes } from './shared/pipes/application.pipes';
-import { ValidatorsModule, EmailValidators} from 'ngx-validators'
-import { AlertaModalComponent } from './shared/modal-alerta/alerta.component';
-import { LayerModalComponent } from './shared/modal-packing/layer.component';
-import { ModalDeleteComponent } from './shared/modal-delete/modal-delete.component';
-import { DashboardModuleAdmin } from './dashboard/dashboard.module';
-import { ChatService } from './servicos/teste';
-import { NguiMapModule } from '@ngui/map';
-import { AppRoutingModule } from './app.routing.module';
-import { TextMaskModule } from 'angular2-text-mask';
-import { AlertModule } from 'ngx-bootstrap/alert';
-import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown/angular2-multiselect-dropdown';
-import { ToastyModule } from 'ng2-toasty';
-import { NouisliderModule } from 'ng2-nouislider';
-import { ServicesModule, AuthInterceptor } from './servicos/service.module';
-import { constants } from './../environments/constants';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { TooltipModule } from 'ngx-bootstrap' 
-import { AlertaAusenteComponent } from './shared/modal-alerta/alerta-ausente/alerta-ausente.component';
-import { AlertaBateriaBaixaComponent } from './shared/modal-alerta/alerta-bateria-baixa/alerta-bateria-baixa.component';
-import { AlertaEmbalagemAtrasadaComponent } from './shared/modal-alerta/alerta-embalagem-atrasada/alerta-embalagem-atrasada.component';
-import { AlertaLocalIncorretoComponent } from './shared/modal-alerta/alerta-local-incorreto/alerta-local-incorreto.component';
-import { AlertaPermanenciaComponent } from './shared/modal-alerta/alerta-permanencia/alerta-permanencia.component';
-import { AlertaEmbalagemPerdidaComponent } from './shared/modal-alerta/alerta-embalagem-perdida/alerta-embalagem-perdida.component';
-import { SidebarModule } from 'ng-sidebar';
-import { CreateUserComponent } from './shared/modal-user/create-user/create-user.component';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { EditUserComponent } from './shared/modal-user/edit-user/edit-user.component'; 
-import { AlertaSemSinalComponent } from './shared/modal-alerta/alerta-sem-sinal/alerta-sem-sinal.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule, FormControl } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpModule } from "@angular/http";
+import {
+  NgbModule,
+  NgbModal,
+  NgbActiveModal,
+  NgbModalOptions
+} from "@ng-bootstrap/ng-bootstrap";
+import {
+  NgProgressBrowserXhr,
+  NgProgressModule,
+  NgProgressInterceptor
+} from "ngx-progressbar";
+import { NgxPaginationModule } from "ngx-pagination";
+import { JWBootstrapSwitchModule } from "jw-bootstrap-switch-ng2";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import * as $ from "jquery";
+import { AppComponent } from "./app.component";
+import { ModalRastComponent } from "./shared/modal-rast/modal-rast.component";
+import { ModalUserComponent } from "./shared/modal-user/modal-user.component";
+import { ModalSupplierRegisterComponent } from "./shared/modal-user/modal-register-supplier/modal-register-supplier.component";
+import { ModalLogisticRegisterComponent } from "./shared/modal-user/modal-register-logistic/modal-register-logistic.component";
+import { ModalStaffRegisterComponent } from "./shared/modal-user/modal-register-staff/modal-register-staff.component";
+import { ModalSupplierEditarComponent } from "./shared/modal-user/modal-editar-supplier/modal-editar-supplier.component";
+import { ModalLogisticEditarComponent } from "./shared/modal-user/modal-editar-logistic/modal-editar-logistic.component";
+import { ModalCurrentEditarComponent } from "./shared/modal-current-edit/modal-editar-current.component";
+import { ModalStaffEditarComponent } from "./shared/modal-user/modal-editar-staff/modal-editar-staff.component";
+import { AbscenseModalComponent } from "./shared/modal-packing-absence/abscense.component";
+import { ModalSettings } from "./shared/modal-settings/modal-settings.component";
+import { ModalInvComponent } from "./shared/modal-inv/modal-inv.component";
+import { ApplicationPipes } from "./shared/pipes/application.pipes";
+import { ValidatorsModule, EmailValidators } from "ngx-validators";
+import { AlertaModalComponent } from "./shared/modal-alerta/alerta.component";
+import { LayerModalComponent } from "./shared/modal-packing/layer.component";
+import { ModalDeleteComponent } from "./shared/modal-delete/modal-delete.component";
+import { DashboardModuleAdmin } from "./dashboard/dashboard.module";
+import { ChatService } from "./servicos/teste";
+import { NguiMapModule } from "@ngui/map";
+import { AppRoutingModule } from "./app.routing.module";
+import { TextMaskModule } from "angular2-text-mask";
+import { AlertModule } from "ngx-bootstrap/alert";
+import { AngularMultiSelectModule } from "angular2-multiselect-dropdown/angular2-multiselect-dropdown";
+import { ToastyModule } from "ng2-toasty";
+import { NouisliderModule } from "ng2-nouislider";
+import { ServicesModule, AuthInterceptor } from "./servicos/service.module";
+import { constants } from "./../environments/constants";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+import { TooltipModule } from "ngx-bootstrap";
+import { AlertaAusenteComponent } from "./shared/modal-alerta/alerta-ausente/alerta-ausente.component";
+import { AlertaBateriaBaixaComponent } from "./shared/modal-alerta/alerta-bateria-baixa/alerta-bateria-baixa.component";
+import { AlertaEmbalagemAtrasadaComponent } from "./shared/modal-alerta/alerta-embalagem-atrasada/alerta-embalagem-atrasada.component";
+import { AlertaLocalIncorretoComponent } from "./shared/modal-alerta/alerta-local-incorreto/alerta-local-incorreto.component";
+import { AlertaPermanenciaComponent } from "./shared/modal-alerta/alerta-permanencia/alerta-permanencia.component";
+import { AlertaEmbalagemPerdidaComponent } from "./shared/modal-alerta/alerta-embalagem-perdida/alerta-embalagem-perdida.component";
+import { SidebarModule } from "ng-sidebar";
+import { CreateUserComponent } from "./shared/modal-user/create-user/create-user.component";
+import { NgSelectModule } from "@ng-select/ng-select";
+import { EditUserComponent } from "./shared/modal-user/edit-user/edit-user.component";
+import { AlertaSemSinalComponent } from "./shared/modal-alerta/alerta-sem-sinal/alerta-sem-sinal.component";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -79,8 +89,7 @@ import { AlertaSemSinalComponent } from './shared/modal-alerta/alerta-sem-sinal/
     AbscenseModalComponent,
 
     CreateUserComponent,
-    EditUserComponent,
-    
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
@@ -93,7 +102,7 @@ import { AlertaSemSinalComponent } from './shared/modal-alerta/alerta-sem-sinal/
     NgbModule.forRoot(),
     AlertModule.forRoot(),
     ToastyModule.forRoot(),
-    BsDatepickerModule.forRoot(), 
+    BsDatepickerModule.forRoot(),
     TooltipModule.forRoot(),
     SidebarModule.forRoot(),
     NgSelectModule,
@@ -108,10 +117,10 @@ import { AlertaSemSinalComponent } from './shared/modal-alerta/alerta-sem-sinal/
     NgxPaginationModule,
     ApplicationPipes,
     NguiMapModule.forRoot({
-      apiUrl: `https://maps.google.com/maps/api/js?key=${constants.GOOGLE_API_KEY}` +
-      '&libraries=visualization,places,drawing',
+      apiUrl:
+        `https://maps.google.com/maps/api/js?key=${constants.GOOGLE_API_KEY}` +
+        "&libraries=visualization,places,drawing"
     })
-
   ],
   providers: [
     ChatService,
@@ -124,8 +133,8 @@ import { AlertaSemSinalComponent } from './shared/modal-alerta/alerta-sem-sinal/
       provide: HTTP_INTERCEPTORS,
       useClass: NgProgressInterceptor,
       multi: true
-    }
-
+    },
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ],
   bootstrap: [AppComponent],
   exports: [ReactiveFormsModule],
@@ -138,16 +147,16 @@ import { AlertaSemSinalComponent } from './shared/modal-alerta/alerta-sem-sinal/
     AlertaPermanenciaComponent,
     AlertaEmbalagemPerdidaComponent,
     AlertaSemSinalComponent,
-    ModalRastComponent, 
-    AbscenseModalComponent, 
-    ModalSettings, 
+    ModalRastComponent,
+    AbscenseModalComponent,
+    ModalSettings,
     ModalCurrentEditarComponent,
     LayerModalComponent,
-    ModalStaffEditarComponent, 
-    ModalUserComponent, 
+    ModalStaffEditarComponent,
+    ModalUserComponent,
     ModalStaffRegisterComponent,
     ModalLogisticEditarComponent,
-    ModalInvComponent, 
+    ModalInvComponent,
     ModalLogisticRegisterComponent,
     ModalDeleteComponent,
     ModalSupplierRegisterComponent,
@@ -157,5 +166,4 @@ import { AlertaSemSinalComponent } from './shared/modal-alerta/alerta-sem-sinal/
     EditUserComponent
   ]
 })
-
-export class AppModule { }
+export class AppModule {}
