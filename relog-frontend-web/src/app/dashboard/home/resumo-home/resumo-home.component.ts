327b1ffd30dbdb6d5ffc9068444f6caee8a430ec
emanoel.silva <emanoel.silva@pe.senai.br>
2018-12-20 18:23:02 -0300

    fix barra de progresso na home

--- relog-frontend-web/src/app/dashboard/home/resumo-home/resumo-home.component.ts
+++ relog-frontend-web/src/app/dashboard/home/resumo-home/resumo-home.component.ts
@@ -30,12 +30,15 @@ export class ResumoHomeComponent implements OnInit {
 
       //Categoria em pontos de controle
       this.progressControle.push((parseFloat(this.resume.qtd_in_cp) / parseFloat(this.resume.qtd_total)) * 100);
-      this.progressControle.push(100 - this.resume.qtd_in_cp);
+      this.progressControle.push(100 - this.progressControle[0]);
 
       //Categoria em viagem
-      this.progressViagem.push(parseFloat(this.resume.qtd_in_cp));
-      this.progressViagem.push((parseFloat(this.resume.qtd_in_traveling) / parseFloat(this.resume.quantityTotal)) * 100);
-      this.progressViagem.push(100 - this.resume.qtd_in_traveling);
+      this.progressViagem.push(this.progressControle[0]);
+      this.progressViagem.push((parseFloat(this.resume.qtd_in_traveling) / parseFloat(this.resume.qtd_total)) * 100);
+      this.progressViagem.push(100 - (this.progressViagem[0] + this.progressViagem[1]));
+
+      console.log(this.progressControle);
+      console.log(this.progressViagem);
     }
   }
 
