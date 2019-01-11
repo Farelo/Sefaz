2ca1d27e2e3ac607a7bd438eb56987d49339f1ab
emanoel.silva <emanoel.silva@pe.senai.br>
2019-01-11 15:53:00 -0300

    inicio de importar dados

--- relog-frontend-web/src/app/servicos/import.service.ts
+++ relog-frontend-web/src/app/servicos/import.service.ts
@@ -13,29 +13,154 @@ export class ImportService {
       return Observable.throw(error);
   }
 
-  sendDataToImportTag(file: any): Observable<any> {
-    return this.http.post(`${environment.url}upload/tag`, file)
-      .catch(this.handleError);
-  }
+  sendDataToImportControlPoint(file: any): Observable<any> {
+    // return this.http.post(`${environment.url}upload/plant`, file)
+    //   .catch(this.handleError);
+    let result = {
+      errors: [
+        {
+          line: 1,
+          description: "Erro por motivo x"
+        },
+        {
+          line: 2,
+          description: "Erro por motivo y"
+        }
+      ],
+      updated: [
+        {
+          line: 3,
+          data: {
+            geofence_type: "C",
+            coordinates: "(11.222, 23.444)",
+            radius: 250,
+            name: "Nome 1",
+            full_address: "Endereço 1",
+            type: "Fábrica",
+            company: "Transporte Nogueira",
+            duns: "11223344",
+          }
+        },
+        {
+          line: 4,
+          data: {
+            geofence_type: "C",
+            coordinates: "(11.222, 23.444)",
+            radius: 250,
+            name: "Nome 2",
+            full_address: "Endereço 2",
+            type: "Fábrica",
+            company: "Transporte Lima",
+            duns: "22003",
+          }
+        }
+      ],
+      to_register: [
+        {
+          line: 5,
+          data: {
+            geofence_type: "C",
+            coordinates: "(11.222, 23.444)",
+            radius: 250,
+            name: "Nome 3",
+            full_address: "Endereço 3",
+            type: "Fábrica",
+            company: "Transporte Rapidex",
+            duns: "4488",
+          }
+        }
+      ]
+    }
 
-  sendDataToImportProject(file: any): Observable<any> {
-    return this.http.post(`${environment.url}upload/project`, file)
-      .catch(this.handleError);
-  }
-
-  sendDataToImportPlant(file: any): Observable<any> {
-    return this.http.post(`${environment.url}upload/plant`, file)
-      .catch(this.handleError);
-  }
-
-  sendDataToImportDepartment(file: any): Observable<any> {
-    return this.http.post(`${environment.url}upload/department`, file)
-      .catch(this.handleError);
+    return Observable.of(result);
   }
 
   sendDataToImportPacking(file: any): Observable<any> {
-    return this.http.post(`${environment.url}upload/packing`, file)
-      .catch(this.handleError);
+    // return this.http.post(`${environment.url}upload/packing`, file)
+    //   .catch(this.handleError);
+
+// <th>family</th>
+// <th>serial</th>
+// <th></th>
+// <th></th>
+// <th></th>
+// <th></th>
+// <th></th>
+// <th></th>
+// <th></th>
+// <th></th>
+// <th></th>
+// <th></th>
+// <th></th>
+
+    let result = {
+      errors: [
+        {
+          line: 1,
+          description: "Erro por motivo x"
+        },
+        {
+          line: 2,
+          description: "Erro por motivo y"
+        }
+      ],
+      updated: [
+        {
+          line: 3,
+          data: {
+            family: "C",
+            serial: 250,
+            tag: "Nome 1",
+            version: "Endereço 1",
+            manufactorer: "Alumynium",
+            weigth: "1.0",
+            width: "1.2",
+            height: "1.3",
+            length: "1.6",
+            capacity: "12",
+            observations: "NENHUMA",
+            type: "Rack metálico",
+            projec: "Fase 2",
+          }
+        },
+        {
+          line: 4,
+          data: {
+            family: "C", 
+            serial: 250,
+            tag: "Nome 2",
+            version: "Endereço 2",
+            manufactorer: "Acme Racks",
+            weigth: "1.0",
+            width: "1.2",
+            height: "1.3",
+            length: "1.6",
+            capacity: "12",
+            observations: "NENHUMA",
+            type: "Rack metálico",
+            projec: "Fase 2",
+          }
+        }
+      ],
+      to_register: [
+        {
+          line: 5,
+          data: {
+            geofence_type: "C",
+            coordinates: "(11.222, 23.444)",
+            radius: 250,
+            name: "Nome 3",
+            full_address: "Endereço 3",
+            type: "Fábrica",
+            company: "Transporte Rapidex",
+            duns: "4488",
+          }
+        }
+      ]
+    }
+
+    return Observable.of(result);
+    
   }
 
   sendDataToImportRoute(file: any): Observable<any> {
