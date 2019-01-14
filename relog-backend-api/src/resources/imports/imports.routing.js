328eea7da658cea554d11ea240670c10b23d2681
giorgiosaints <giorgiosaints@gmail.com>
2019-01-14 11:42:02 -0300

    Importar dados CP adicionado

--- relog-backend-api/src/resources/imports/imports.routing.js
+++ relog-backend-api/src/resources/imports/imports.routing.js
@@ -4,6 +4,7 @@ const import_controller = require('./imports.controller')
 const auth = require('../../security/auth.middleware')
 
 router.post('/packing_xlsx', auth, import_controller.import_packing)
+router.post('/control_point_xlsx', auth, import_controller.import_control_points)
 
 module.exports = router
 
@@ -26,6 +27,40 @@ module.exports = router
  *     parameters:
  *       - in: formData
  *         name: packing_xlsx
+ *         description: The file to upload xlsx
+ *         required: true
+ *         type: file
+ *     responses:
+ *       200:
+ *         description: Family is valid request
+ *       400:
+ *         description: Bad Request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not Found
+ *
+ */
+
+// POST '/'
+/**
+ * @swagger
+ *
+ * /imports/control_point_xlsx:
+ *   post:
+ *     summary: Uploads xlsx file to create control points
+ *     description: Uploads xlsx file to create control points
+ *     security:
+ *       - Bearer: []
+ *     tags:
+ *       - Import
+ *     consumes:
+ *       - multipart/form-data
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: formData
+ *         name: control_point_xlsx
  *         description: The file to upload xlsx
  *         required: true
  *         type: file
