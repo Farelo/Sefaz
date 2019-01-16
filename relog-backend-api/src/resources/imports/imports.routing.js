fca2877394dd01a59bb986f2beeb24ba63967087
giorgiosaints <giorgiosaints@gmail.com>
2019-01-16 12:29:40 -0300

    Company agora tem importar data

--- relog-backend-api/src/resources/imports/imports.routing.js
+++ relog-backend-api/src/resources/imports/imports.routing.js
@@ -5,6 +5,7 @@ const auth = require('../../security/auth.middleware')
 
 router.post('/packing_xlsx', auth, import_controller.import_packing)
 router.post('/control_point_xlsx', auth, import_controller.import_control_points)
+router.post('/company_xlsx', auth, import_controller.import_companies)
 
 module.exports = router
 
@@ -61,6 +62,40 @@ module.exports = router
  *     parameters:
  *       - in: formData
  *         name: control_point_xlsx
+ *         description: The file to upload xlsx
+ *         required: true
+ *         type: file
+ *     responses:
+ *       200:
+ *         description: Family is valid request
+ *       400:
+ *         description: Bad Request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not Found
+ *
+ */
+
+// POST '/'
+/**
+ * @swagger
+ *
+ * /imports/company_xlsx:
+ *   post:
+ *     summary: Uploads xlsx file to create companies
+ *     description: Uploads xlsx file to create control points
+ *     security:
+ *       - Bearer: []
+ *     tags:
+ *       - Import
+ *     consumes:
+ *       - multipart/form-data
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: formData
+ *         name: company_xlsx
  *         description: The file to upload xlsx
  *         required: true
  *         type: file
