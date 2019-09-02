cd727d69491c149460f5471635d8960aa27ce7ca
Ricardo Robson <ricardo.mendes@sistemafiepe.org.br>
2019-09-02 15:05:09 -0300

    Ident

--- websocket/src/db/models/device_data.model.js
+++ websocket/src/db/models/device_data.model.js
@@ -1,182 +1,186 @@
-const mongoose = require('mongoose')
-const { Packing } = require('./packings.model')
-const logger = require('../../config/winston.config')
+const mongoose = require("mongoose");
+const { Packing } = require("./packings.model");
+const logger = require("../../config/winston.config");
 
 const deviceDataSchema = new mongoose.Schema({
-    device_id: {
-        type: String,
-        required: true
+  device_id: {
+    type: String,
+    required: true
+  },
+  message_date: {
+    type: Date,
+    required: true
+  },
+  message_type: {
+    type: String,
+    required: true
+  },
+  message_date_timestamp: {
+    type: Number,
+    required: true
+  },
+  last_communication: {
+    type: Date
+  },
+  last_communication_timestamp: {
+    type: Number
+  },
+  latitude: {
+    type: Number
+  },
+  longitude: {
+    type: Number
+  },
+  accuracy: {
+    type: Number
+  },
+  temperature: {
+    type: Number
+  },
+  seq_number: {
+    type: Number
+  },
+  battery: {
+    percentage: {
+      type: Number
     },
-    message_date: {
-        type: Date,
-        required: true
-    },
-    message_type: {
-        type: String,
-        required: true
-    },
-    message_date_timestamp: {
-        type: Number,
-        required: true
-    },
-    last_communication: {
-        type: Date
-    },
-    last_communication_timestamp: {
-        type: Number
-    },
-    latitude: {
-        type: Number
-    },
-    longitude: {
-        type: Number
-    },
-    accuracy: {
-        type: Number
-    },
-    temperature: {
-        type: Number
-    },
-    seq_number: {
-        type: Number
-    },
-    battery: {
-        percentage: {
-            type: Number
-        },
-        voltage: {
-            type: Number
-        },
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-
-    },
-    update_at: {
-        type: Date,
-        default: Date.now
-    },
-    message: {
-        type: String
+    voltage: {
+      type: Number
     }
-})
-
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  update_at: {
+    type: Date,
+    default: Date.now
+  },
+  message: {
+    type: String
+  }
+});
 
 //deviceDataSchema.index({ device_id: 1, message_date: -1, message_type: 1 }, { unique: true })
 
 const update_packing = async (device_data, next) => {
-    logger.info("Update packing after save DeviceData")
-    try {
-        let update_attrs = {}
-        let update = false
-        const tag = { code: device_data.device_id }
-        const packing = await Packing.findByTag(tag)
-    
-        if (!packing) next()
+  logger.info("Update packing after save DeviceData");
+  try {
+    let update_attrs = {};
+    let update = false;
+    const tag = { code: device_data.device_id };
+    const packing = await Packing.findByTag(tag);
 
-        let current_message_date_on_packing = await DeviceData.findById(packing.last_device_data, {_id: 0, message_date: 1})
+    if (!packing) next();
 
-        current_message_date_on_packing = current_message_date_on_packing ? current_message_date_on_packing.message_date : null
+    let current_message_date_on_packing = await DeviceData.findById(
+      packing.last_device_data,
+      { _id: 0, message_date: 1 }
+    );
 
-        //se o novo device_data é mais recente que o que já esta salvo, então atualiza
-        if(device_data.message_date > current_message_date_on_packing) {
+    current_message_date_on_packing = current_message_date_on_packing
+      ? current_message_date_on_packing.message_date
+      : null;
 
-            update_attrs.last_device_data = device_data._id
+    //se o novo device_data é mais recente que o que já esta salvo, então atualiza
+    if (device_data.message_date > current_message_date_on_packing) {
+      update_attrs.last_device_data = device_data._id;
 
-            update = true
-        }
-
-        //se o novo device_data possui informação de bateria
-        if (device_data.battery.percentage || device_data.battery.voltage) {
-        
-            let packing_date_battery_data = await DeviceData.findById(packing.last_device_data_battery, {_id: 0, message_date: 1})
-
-            packing_date_battery_data = packing_date_battery_data ? packing_date_battery_data.message_date : null
-
-            // se essa informação de bateria é mais recente que a que ja existe no packing ou o packing não tem ainda nenhuma info de bateria
-            if (device_data.message_date > packing_date_battery_data){
-
-                update_attrs.last_device_data_battery =  device_data._id
-            }
-        
-            update = true
-        }
-
-        if (update)
-            await Packing.findByIdAndUpdate(packing._id, update_attrs, { new: true })
-
-        next()
-        logger.info("Update packing after save DeviceData SUCESS!")
-
-    } catch (error) {
-        logger.info("Update packing after save DeviceData ERROR!")
-        logger.info(error)
-
-        next(error)
+      update = true;
     }
-}
 
-deviceDataSchema.statics.findByDeviceId = function (device_id, projection = '') {
-    return this.findOne({ device_id }, projection)
-}
+    //se o novo device_data possui informação de bateria
+    if (device_data.battery.percentage || device_data.battery.voltage) {
+      let packing_date_battery_data = await DeviceData.findById(
+        packing.last_device_data_battery,
+        { _id: 0, message_date: 1 }
+      );
 
-deviceDataSchema.statics.findMosRecentById = function (id) {
-    return this.find({ 'device_id': id}).sort( {_id: -1 }).limit(1)
-}
+      packing_date_battery_data = packing_date_battery_data
+        ? packing_date_battery_data.message_date
+        : null;
 
-const saveDeviceDataToPacking = function (doc, next) {
-    update_packing(doc, next)
-}
+      // se essa informação de bateria é mais recente que a que ja existe no packing ou o packing não tem ainda nenhuma info de bateria
+      if (device_data.message_date > packing_date_battery_data) {
+        update_attrs.last_device_data_battery = device_data._id;
+      }
 
-const update_updated_at_middleware = function (next) {
-    let update = this.getUpdate()
-    update.update_at = new Date()
-    next()
-}
-
-const device_data_save = async (device_data) => {
-    logger.info("Saving DeviceData: "+device_data.device_id)
-    try {
-        const new_device_data = new DeviceData({
-            device_id: device_data.device_id.toString(),
-            message_date: device_data.message_date,
-            message_date_timestamp: device_data.message_date_timestamp,
-            message_type: device_data.message_type,
-            last_communication: device_data.last_communication,
-            last_communication_timestamp: device_data.last_communication_timestamp,
-            latitude: device_data.latitude,
-            longitude: device_data.longitude,
-            accuracy: device_data.accuracy,
-            temperature: device_data.temperature,
-            seq_number: device_data.seq_number,
-            battery: {
-                percentage: device_data.battery.percentage,
-                voltage: device_data.battery.voltage
-            },
-            message: device_data.message
-        })
-
-        //salva no banco | observação: não salva mensagens iguais porque o model possui indice unico e composto por device_id e message_date,
-        //e o erro de duplicidade nao interrompe o job
-        await new_device_data.save( )
-        logger.info("Save DeviceData with Sucess!")
-
-
-        // debug('Saved device_data ', device_data.deviceId, ' and message_date ', device_data.messageDate)
-
-    } catch (error) {
-        logger.info("Error to save DeviceData!")
-        logger.info(error)
+      update = true;
     }
-}
 
-deviceDataSchema.post('save', saveDeviceDataToPacking)
-deviceDataSchema.pre('update', update_updated_at_middleware)
-deviceDataSchema.pre('findOneAndUpdate', update_updated_at_middleware)
+    if (update)
+      await Packing.findByIdAndUpdate(packing._id, update_attrs, { new: true });
 
-const DeviceData = mongoose.model('DeviceData', deviceDataSchema)
+    next();
+    logger.info("Update packing after save DeviceData SUCESS!");
+  } catch (error) {
+    logger.info("Update packing after save DeviceData ERROR!");
+    logger.info(error);
 
-exports.DeviceData = DeviceData
-exports.deviceDataSchema = deviceDataSchema
-exports.device_data_save = device_data_save
+    next(error);
+  }
+};
+
+deviceDataSchema.statics.findByDeviceId = function(device_id, projection = "") {
+  return this.findOne({ device_id }, projection);
+};
+
+deviceDataSchema.statics.findMosRecentById = function(id) {
+  return this.find({ device_id: id })
+    .sort({ _id: -1 })
+    .limit(1);
+};
+
+const saveDeviceDataToPacking = function(doc, next) {
+  update_packing(doc, next);
+};
+
+const update_updated_at_middleware = function(next) {
+  let update = this.getUpdate();
+  update.update_at = new Date();
+  next();
+};
+
+const device_data_save = async device_data => {
+  logger.info("Saving DeviceData: " + device_data.device_id);
+  try {
+    const new_device_data = new DeviceData({
+      device_id: device_data.device_id.toString(),
+      message_date: device_data.message_date,
+      message_date_timestamp: device_data.message_date_timestamp,
+      message_type: device_data.message_type,
+      last_communication: device_data.last_communication,
+      last_communication_timestamp: device_data.last_communication_timestamp,
+      latitude: device_data.latitude,
+      longitude: device_data.longitude,
+      accuracy: device_data.accuracy,
+      temperature: device_data.temperature,
+      seq_number: device_data.seq_number,
+      battery: {
+        percentage: device_data.battery.percentage,
+        voltage: device_data.battery.voltage
+      },
+      message: device_data.message
+    });
+
+    //salva no banco | observação: não salva mensagens iguais porque o model possui indice unico e composto por device_id e message_date,
+    //e o erro de duplicidade nao interrompe o job
+    await new_device_data.save();
+    logger.info("Save DeviceData with Sucess!");
+
+    // debug('Saved device_data ', device_data.deviceId, ' and message_date ', device_data.messageDate)
+  } catch (error) {
+    logger.info("Error to save DeviceData!");
+    logger.info(error);
+  }
+};
+
+deviceDataSchema.post("save", saveDeviceDataToPacking);
+deviceDataSchema.pre("update", update_updated_at_middleware);
+deviceDataSchema.pre("findOneAndUpdate", update_updated_at_middleware);
+
+const DeviceData = mongoose.model("DeviceData", deviceDataSchema);
+
+exports.DeviceData = DeviceData;
+exports.deviceDataSchema = deviceDataSchema;
+exports.device_data_save = device_data_save;
--- websocket/src/webSocket.js
+++ websocket/src/webSocket.js
@@ -1,37 +1,44 @@
-const logger = require('./config/winston.config')
+const logger = require("./config/winston.config");
 var https = require("https");
 var WebSocketClient = require("websocket").client;
 var client = new WebSocketClient();
-const { Packing } = require('./db/models/packings.model')
-const {DeviceData, device_data_save} = require('./db/models/device_data.model')
-const { parserMessage } = require('./util/parserMessage')
+const { Packing } = require("./db/models/packings.model");
+const {
+  DeviceData,
+  device_data_save
+} = require("./db/models/device_data.model");
+const { parserMessage } = require("./util/parserMessage");
 
 //Authentication Token
 //Token of DM
-var token = "bb1ab275-2985-461b-8766-10c4b2c4127a"
+var token = "bb1ab275-2985-461b-8766-10c4b2c4127a";
 
 // Getting Dict DevicesIds x last DeviceData
-let deviceDictList
-async function getDeviceDictList(){
-    await require('./db/db')()
-    
-    logger.info("Getting Dict DevicesIds x last DeviceData")
+let deviceDictList;
+async function getDeviceDictList() {
+  await require("./db/db")();
 
-    //Getting all DeviceIds
-    let result = await Packing.find({}, {_id: 0, tag: 1})
-                                .then(packFind =>{
-                                    let packMapped = packFind.filter(packFilter => {
-                                            return packFilter.tag.code != undefined
-                                        }).map(async packMap => {
-                                            //Get last deviceData from DB
-                                            let lastDeviceData = await DeviceData.find({ 'device_id': packMap.tag.code}).sort( {_id: -1}).limit(1)
-                                                                        .then(resultFind =>{
-                                                                            return resultFind[0]
-                                                                        })
-                                            
+  logger.info("Getting Dict DevicesIds x last DeviceData");
 
-                                            //Get last deviceData from mock empty
-                                            /*
+  //Getting all DeviceIds
+  let result = await Packing.find({}, { _id: 0, tag: 1 }).then(packFind => {
+    let packMapped = packFind
+      .filter(packFilter => {
+        return packFilter.tag.code != undefined;
+      })
+      .map(async packMap => {
+        //Get last deviceData from DB
+        let lastDeviceData = await DeviceData.find({
+          device_id: packMap.tag.code
+        })
+          .sort({ _id: -1 })
+          .limit(1)
+          .then(resultFind => {
+            return resultFind[0];
+          });
+
+        //Get last deviceData from mock empty
+        /*
                                             let lastDeviceData = {
                                                 device_id: packMap.tag.code,
                                                 message_date: null,
@@ -51,137 +58,154 @@ async function getDeviceDictList(){
                                                 message: null
                                             }
                                             */
-                                           
-                                            let dict = {
-                                                deviceId: packMap.tag.code,
-                                                lastDeviceData: lastDeviceData
-                                            }
-                                            return dict
-                                        })
-                                    
-                                    return packMapped
-                                })
-    deviceDictList = await Promise.all(result)
-    return deviceDictList    
+
+        let dict = {
+          deviceId: packMap.tag.code,
+          lastDeviceData: lastDeviceData
+        };
+        return dict;
+      });
+
+    return packMapped;
+  });
+  deviceDictList = await Promise.all(result);
+  return deviceDictList;
 }
 
-
 // Subscribing Devices in Websocket
-async function subscribingDeviceIds(deviceDictList){
-    logger.info("Subscribing Devices in Websocket")
+async function subscribingDeviceIds(deviceDictList) {
+  logger.info("Subscribing Devices in Websocket");
 
-    deviceDictList.forEach(async deviceDict => {
-        logger.info("Subscribing device with id "+deviceDict.deviceId)
-        var optionsget = {
-            host: "core.loka.systems",
-            port: 443,
-            path: "/subscribe_terminal/" + deviceDict.deviceId,
-            method: "GET",
-            headers: { Authorization: "Bearer " + token }
-        };
-    
-        // do the GET request
-        var reqGet = https.request(optionsget, function(res) {
-            logger.info("Response code on subscribing of device with id "+deviceDict.deviceId+": "+res.statusCode)
-            res.on("data", function(d) {
-                logger.info("GET result:\n"+d);
-            });
-        });
-    
-        reqGet.end();
-        reqGet.on("error", function(e) {
-            logger.info("Error on subscribing of device with id "+deviceDict.deviceId+": "+e)
-            logger.info(e);
-        });
+  deviceDictList.forEach(async deviceDict => {
+    logger.info("Subscribing device with id " + deviceDict.deviceId);
+    var optionsget = {
+      host: "core.loka.systems",
+      port: 443,
+      path: "/subscribe_terminal/" + deviceDict.deviceId,
+      method: "GET",
+      headers: { Authorization: "Bearer " + token }
+    };
+
+    // do the GET request
+    var reqGet = https.request(optionsget, function(res) {
+      logger.info(
+        "Response code on subscribing of device with id " +
+          deviceDict.deviceId +
+          ": " +
+          res.statusCode
+      );
+      res.on("data", function(d) {
+        logger.info("GET result:\n" + d);
+      });
     });
+
+    reqGet.end();
+    reqGet.on("error", function(e) {
+      logger.info(
+        "Error on subscribing of device with id " +
+          deviceDict.deviceId +
+          ": " +
+          e
+      );
+      logger.info(e);
+    });
+  });
 }
 
 // Unsubscribing Devices in Websocket
-async function unsubscribingDeviceIds(deviceDictList){
-    logger.info("Unsubscribing Devices in Websocket")
+async function unsubscribingDeviceIds(deviceDictList) {
+  logger.info("Unsubscribing Devices in Websocket");
 
-    deviceDictList.forEach(async deviceDict => {
-        logger.info("Unsubscribing device with id "+deviceDict.deviceId)
-        var optionsget = {
-            host: "core.loka.systems",
-            port: 443,
-            path: "/unsubscribe_terminal/" + deviceDict.deviceId,
-            method: "GET",
-            headers: { Authorization: "Bearer " + token }
-        };
-    
-        // do the GET request
-        var reqGet = https.request(optionsget, function(res) {
-            logger.info("Response code on Unsubscribing of device with id "+deviceDict.deviceId+": "+res.statusCode)
-            res.on("data", function(d) {
-                logger.info("GET result:\n"+d);
-            });
-        });
-    
-        reqGet.end();
-        reqGet.on("error", function(e) {
-            logger.info("Error on Unsubscribing of device with id "+deviceDict.deviceId+": "+e)
-            logger.info(e);
-        });
+  deviceDictList.forEach(async deviceDict => {
+    logger.info("Unsubscribing device with id " + deviceDict.deviceId);
+    var optionsget = {
+      host: "core.loka.systems",
+      port: 443,
+      path: "/unsubscribe_terminal/" + deviceDict.deviceId,
+      method: "GET",
+      headers: { Authorization: "Bearer " + token }
+    };
+
+    // do the GET request
+    var reqGet = https.request(optionsget, function(res) {
+      logger.info(
+        "Response code on Unsubscribing of device with id " +
+          deviceDict.deviceId +
+          ": " +
+          res.statusCode
+      );
+      res.on("data", function(d) {
+        logger.info("GET result:\n" + d);
+      });
     });
+
+    reqGet.end();
+    reqGet.on("error", function(e) {
+      logger.info(
+        "Error on Unsubscribing of device with id " +
+          deviceDict.deviceId +
+          ": " +
+          e
+      );
+      logger.info(e);
+    });
+  });
 }
 
 // Start and manage the WebSocket
-function initWebSocket(){
-    client.on("connectFailed", function(error) {
-        logger.info("WebSocket Connect Failed: " + error.toString());
-    });
-    
-    client.on("connect", function(connection) {
-        logger.info("WebSocket Client Connected");
-    
-        connection.on("error", function(error) {
-            logger.info("WebSocket Connection Error: " + error.toString())
-            initWebSocket()
-        });
-    
-        connection.on("close", function() {
-            logger.info("WebSocket Echo-protocol Connection Closed")
-            initWebSocket()
-        });
-    
-        connection.on("message", async function(message) {
-            if (message.type === "utf8") {
-                logger.info("WebSocket Received: '" + message.utf8Data + "'");
-                
-                let jsonMessage = JSON.parse(message.utf8Data)
-                
-                let deviceDict = deviceDictList.find(function(elem) { 
-                    return elem.deviceId==jsonMessage.src; 
-                }); 
+function initWebSocket() {
+  client.on("connectFailed", function(error) {
+    logger.info("WebSocket Connect Failed: " + error.toString());
+  });
 
-                let deviceData = deviceDict.lastDeviceData
-                
-                let deviceDataToSave = await parserMessage(jsonMessage, deviceData)
-                if(deviceDataToSave !== null){
-                    await device_data_save(deviceDataToSave)
-                    deviceData = deviceDataToSave
-                }                
-            }
-        });
+  client.on("connect", function(connection) {
+    logger.info("WebSocket Client Connected");
+
+    connection.on("error", function(error) {
+      logger.info("WebSocket Connection Error: " + error.toString());
+      initWebSocket();
     });
-    
-    client.connect(
-        "wss://core.loka.systems/messages",
-        null,
-        null,
-        { Authorization: "Bearer " + token },
-        null
-    );
+
+    connection.on("close", function() {
+      logger.info("WebSocket Echo-protocol Connection Closed");
+      initWebSocket();
+    });
+
+    connection.on("message", async function(message) {
+      if (message.type === "utf8") {
+        logger.info("WebSocket Received: '" + message.utf8Data + "'");
+
+        let jsonMessage = JSON.parse(message.utf8Data);
+
+        let deviceDict = deviceDictList.find(function(elem) {
+          return elem.deviceId == jsonMessage.src;
+        });
+
+        let deviceData = deviceDict.lastDeviceData;
+
+        let deviceDataToSave = await parserMessage(jsonMessage, deviceData);
+        if (deviceDataToSave !== null) {
+          await device_data_save(deviceDataToSave);
+          deviceData = deviceDataToSave;
+        }
+      }
+    });
+  });
+
+  client.connect(
+    "wss://core.loka.systems/messages",
+    null,
+    null,
+    { Authorization: "Bearer " + token },
+    null
+  );
 }
-
-
 
 const runWS = async () => {
-    await getDeviceDictList()    
-    //await subscribingDeviceIds(deviceDictList)
-    //await unsubscribingDeviceIds(deviceDictList)
-    await initWebSocket()
-}
+  await getDeviceDictList();
+  //await subscribingDeviceIds(deviceDictList)
+  //await unsubscribingDeviceIds(deviceDictList)
+  await initWebSocket();
+};
 
-exports.runWS = runWS
+exports.runWS = runWS;
