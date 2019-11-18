cca949d1d0f9b588247c4b8e4f15dec6c0f40268
Ricardo Robson <rcrdrobson@gmail.com>
2019-11-18 11:27:17 -0300

    giro

--- relog-backend-api/src/resources/reports/reports.repository.js
+++ relog-backend-api/src/resources/reports/reports.repository.js
@@ -215,6 +215,11 @@ exports.snapshot_report = async () => {
                 obj.accuracy = packing.last_device_data ? packing.last_device_data.accuracy : '-'
                 obj.lat_lng_device = await getLatLngOfPacking(packing)
 
+                
+                obj.cicle_start = packing.cicle_start ? packing.cicle_start : '-'
+                obj.cicle_end = packing.cicle_end ? packing.cicle_end : '-'
+                obj.last_cicle_duration = packing.last_cicle_duration ? packing.last_cicle_duration : '-'
+                
                 obj.lat_lng_cp = '-'
                 obj.cp_type = '-'
                 obj.cp_name = '-'
@@ -367,10 +372,6 @@ exports.snapshot_recovery_report = async (snapshot_date) => {
                 obj.serial = packing.serial
                 obj.tag = packing.tag.code 
                 obj.collect_date = snapshot_date
-                
-                obj.cicle_start = packing.cicle_start ? packing.cicle_start : '-'
-                obj.cicle_end = packing.cicle_end ? packing.cicle_end : '-'
-                obj.last_cicle_duration = packing.last_cicle_duration ? packing.last_cicle_duration : '-'
 
                 // obj.last_elegible_accuracy = '-'
                 // obj.last_elegible_lat_lng_device = '-'
