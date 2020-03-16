e699a45104e156c8aacafee61ce356f014b141ab
Emanoel Carlos Gomes Ferraz Silva <emanoel.silva@pe.senai.br>
2020-03-16 14:51:55 -0300

    ajuste ao limpar o filtro no inventario permanencia

--- relog-frontend-web/src/app/dashboard/inventario/inventario-permanencia/inventario-permanencia.component.ts
+++ relog-frontend-web/src/app/dashboard/inventario/inventario-permanencia/inventario-permanencia.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
-import { Pagination } from '../../../shared/models/pagination'; 
+import { Pagination } from '../../../shared/models/pagination';
 import { InventoryService, PackingService, AuthenticationService, ReportsService, FamiliesService } from '../../../servicos/index.service';
 import { Angular2Csv } from 'angular2-csv/Angular2-csv';
 import { FloatTimePipe } from '../../../shared/pipes/floatTime';
@@ -14,13 +14,13 @@ declare var jsPDF: any;
 })
 export class InventarioPermanenciaComponent implements OnInit {
 
-  public listOfPermanence: any[] = []; 
-  public auxListOfPermanence: any[] = []; 
+  public listOfPermanence: any[] = [];
+  // public auxListOfPermanence: any[] = [];
 
-  public listOfFamilies: any[] = [];  
+  public listOfFamilies: any[] = [];
   public selectedFamily: any = null;
-  
-  public listOfSerials: any[] = [];  
+
+  public listOfSerials: any[] = [];
   public selectedSerial: any = null;
 
   public actualPage: number = -1;
@@ -43,14 +43,13 @@ export class InventarioPermanenciaComponent implements OnInit {
     //Loads the data in the table
     this.permanenceInventory();
   }
-  
+
   /**
    * Default list
    */
   permanenceInventory() {
     this.reportService.getPermanenceInventory().subscribe(result => {
-      this.listOfPermanence = result; 
-      this.auxListOfPermanence = result; 
+      this.listOfPermanence = result;
     }, err => { console.log(err) });
   }
 
@@ -60,85 +59,65 @@ export class InventarioPermanenciaComponent implements OnInit {
   loadPackings() {
     this.familyService.getAllFamilies().subscribe(result => {
 
-      this.listOfFamilies = result; 
+      this.listOfFamilies = result;
     }, err => console.error(err));
   }
 
   /**
    * A family was selected
    */
-  familyFilter(event: any){
+  familyFilter(event: any) {
     console.log(event);
 
-    if(!event) {
-      this.listOfPermanence = this.auxListOfPermanence;
-      this.selectedFamily = null;
-
-      this.selectedSerial = null;
+    if (!event) {
       this.listOfSerials = [];
-      return;
+      this.selectedSerial = null;
+
+      this.permanenceInventory();
+
+    } else {
+      this.reportService.getPermanenceInventory({ family: this.selectedFamily._id }).subscribe(result => {
+        this.listOfPermanence = result;
+      }, err => { console.log(err) });
+
+      this.loadSerials(event);
     }
-
-    this.listOfPermanence = this.auxListOfPermanence.filter(elem => {
-      return elem.family_code == event.code;
-    });
-
-    this.loadSerials(event);
   }
 
   /**
    * Loads the serials
    */
-  loadSerials(event: any){
+  loadSerials(event: any) {
 
     this.selectedSerial = null;
     console.log(event);
     this.packingService.getAllPackings({ family: event._id }).subscribe(result => {
-
-      this.listOfSerials = result; 
+      this.listOfSerials = result;
     }, err => console.error(err));
-
-    this.reportService.getPermanenceInventory({ family: this.selectedFamily._id }).subscribe(result => {
-      this.listOfPermanence = result; 
-      this.auxListOfPermanence = result; 
-    }, err => { console.log(err) });
-
-    // let aux = this.listOfPermanence.filter(elem =>{
-    //   return elem.family_code == event.code;
-    // });
-
-    // // console.log(aux);
-    // this.listOfSerials = aux;
   }
 
   /**
    * A serial was selected
    */
   serialFilter(event: any) {
-    
-    this.reportService.getPermanenceInventory({ family: this.selectedFamily._id, serial: this.selectedSerial }).subscribe(result => {
-      this.listOfPermanence = result; 
-      this.auxListOfPermanence = result; 
+
+    let filter = {}
+
+    if (!event) {
+      filter = { family: this.selectedFamily._id }
+    } else{
+      filter = { family: this.selectedFamily._id, serial: this.selectedSerial }
+    }
+
+    this.reportService.getPermanenceInventory(filter).subscribe(result => {
+      this.listOfPermanence = result;
     }, err => { console.log(err) });
-
-    // if (!event) {
-    //   this.selectedSerial = null;
-    //   this.familyFilter(this.selectedFamily);
-    //   return;
-    // }
-
-    // let aux = this.auxListOfPermanence.filter(elem => {
-    //   return ((elem.family_code == event.family_code) && (elem.serial == event.serial));
-    // });
-
-    //console.log(aux);
-    // this.listOfPermanence = aux;
   }
 
-   /**
-   * 
-   * Ordenação da tabela
-   */ 
+  /**
+  * 
+  * Ordenação da tabela
+  */
   public headers: any = [];
   public sortStatus: any = ['asc', 'desc'];
   public sort: any = {
@@ -154,18 +133,13 @@ export class InventarioPermanenciaComponent implements OnInit {
     this.headers.push({ label: 'Ponto de Controle Atual', name: 'current_control_point_name' });
     this.headers.push({ label: 'Local', name: 'current_control_point_type' });
     this.headers.push({ label: 'Tempo no Ponto de Controle', name: 'permanence_time_exceeded' });
-
-    //console.log('this.headers: ' + JSON.stringify(this.headers));
   }
 
   headerClick(item: any) {
     this.sort.name = item.name;
     this.sort.order = this.sortStatus[(this.sortStatus.indexOf(this.sort.order) + 1) % 2];
 
-    // console.log('---');
-    // console.log('this.sort: ' + JSON.stringify(this.sort));
-
-    this.listOfPermanence  = this.customSort(this.listOfPermanence , item.name.split("."), this.sort.order);
+    this.listOfPermanence = this.customSort(this.listOfPermanence, item.name.split("."), this.sort.order);
   }
 
   /**
@@ -178,10 +152,6 @@ export class InventarioPermanenciaComponent implements OnInit {
     var sortOrder = 1;
     if (reverse == 'desc') sortOrder = -1;
 
-    // console.log('array.length: ' + array.length);
-    // console.log('keyArr: ' + keyArr);
-    // console.log('sortOrder: ' + sortOrder);
-
     return array.sort(function (a, b) {
       var x = a, y = b;
       for (var i = 0; i < keyArr.length; i++) {
@@ -191,11 +161,11 @@ export class InventarioPermanenciaComponent implements OnInit {
       return sortOrder * ((x < y) ? -1 : ((x > y) ? 1 : 0));
     });
   }
+
   /**
   * ================================================
   * Downlaod csv file
   */
-
   private csvOptions = {
     showLabels: true,
     fieldSeparator: ';'
@@ -204,7 +174,7 @@ export class InventarioPermanenciaComponent implements OnInit {
   /**
   * Click to download
   */
-  downloadCsv(){
+  downloadCsv() {
 
     //Flat the json object to print
     //I'm using the method slice() just to copy the array as value.
@@ -217,10 +187,10 @@ export class InventarioPermanenciaComponent implements OnInit {
     new Angular2Csv(flatObjectData, 'Inventario Equipamento Tempo de permanência', this.csvOptions);
   }
 
-    /**
-   * Click to download pdf file
-   */
-  downloadPdf(){
+  /**
+ * Click to download pdf file
+ */
+  downloadPdf() {
     var doc = jsPDF('l', 'pt');
 
     // You can use html:
@@ -244,26 +214,26 @@ export class InventarioPermanenciaComponent implements OnInit {
   }
 
   flatObject(mArray: any) {
-    
+
     //console.log(mArray);
 
-     let transformer = new FloatTimePipe();
-     let plainArray = mArray.map(obj => {
-          return {
-            a1: obj.family_code,
-            a2: obj.serial,
-            a3: obj.tag.code,
-            a4: obj.current_control_point_name,
-            a5: obj.current_control_point_type,
-            a6: obj.permanence_time_exceeded !== '-' ? transformer.transform(obj.permanence_time_exceeded) : 'Sem histórico',
-          };
-        });
-      
+    let transformer = new FloatTimePipe();
+    let plainArray = mArray.map(obj => {
+      return {
+        a1: obj.family_code,
+        a2: obj.serial,
+        a3: obj.tag.code,
+        a4: obj.current_control_point_name,
+        a5: obj.current_control_point_type,
+        a6: obj.permanence_time_exceeded !== '-' ? transformer.transform(obj.permanence_time_exceeded) : 'Sem histórico',
+      };
+    });
+
     // As my array is already flat, I'm just returning it.
     return plainArray;
   }
 
-  addHeader(mArray: any){
+  addHeader(mArray: any) {
     let cabecalho = {
       a1: 'Família',
       a2: 'Serial',
