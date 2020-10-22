6d1f034532642ca04a3b85f1516103b5b1b5f6a7
Emanoel Carlos <emanoel.carlos@hotmail.com>
2020-10-22 18:12:03 -0300

    fix main

--- relog-state-machine-job/src/scripts/main.script.js
+++ relog-state-machine-job/src/scripts/main.script.js
@@ -1,63 +1,56 @@
-const ora = require('ora')
-const cron = require('node-cron')
+const ora = require("ora");
+const cron = require("node-cron");
 
-const { Setting } = require('../models/settings.model')
-const { Packing } = require('../models/packings.model')
-const { ControlPoint } = require('../models/control_points.model')
+const { Setting } = require("../models/settings.model");
+const { Packing } = require("../models/packings.model");
+const { ControlPoint } = require("../models/control_points.model");
 
-const runSM = require('./runSM.script')
-const spinner = ora('State Machine working...')
+const runSM = require("./runSM.script");
+const spinner = ora("State Machine working...");
 
 module.exports = async () => {
-    
-    let setting = await getSettings()
+   let setting = await getSettings();
 
-    let nextSemaphor = true
-    
-    // cron.schedule(`*/1 * * * *`, async () => { 
+   let nextSemaphor = true;
 
-    //     if (nextSemaphor){
+   cron.schedule(`*/1 * * * *`, async () => {
+      if (nextSemaphor) {
+         //close the semaphor
+         nextSemaphor = false;
 
-    //         //close the semaphor
-    //         nextSemaphor = false
+         setTimeout(async () => {
+            spinner.start();
 
-    //         setTimeout(async () => {
-                
-                spinner.start()
-                
-                console.log('START')
-                console.log((new Date().toISOString()))
+            console.log("START");
+            console.log(new Date().toISOString());
 
-                setting = await getSettings()
+            setting = await getSettings();
 
-                // const device_data_array = await DeviceData.find({})
-                const controlPoints = await ControlPoint.find({ })
-                    .populate('company')
-                    .populate('type')
-                    
-                //const packings = await Packing.find({ }) 
-                const packings = await Packing.find({'tag.code': '4071928'})
-                    .populate('family')
-                    .populate('last_device_data')
-                    .populate('last_device_data_battery')
-                    .populate('last_current_state_history')
-                    .populate('last_event_record')
+            // const device_data_array = await DeviceData.find({})
+            const controlPoints = await ControlPoint.find({}).populate("company").populate("type");
 
-                await iteratePackings(setting, packings, controlPoints)
-            
-                // packings.forEach(packing => {
-                //     runSM(setting, packing, controlPoints)
-                // })
+            //const packings = await Packing.find({ })
+            const packings = await Packing.find({})
+               .populate("family")
+               .populate("last_device_data")
+               .populate("last_device_data_battery")
+               .populate("last_current_state_history")
+               .populate("last_event_record");
 
-                spinner.succeed('Finished!')
+            await iteratePackings(setting, packings, controlPoints);
 
-                //open the semmaphor
-                nextSemaphor = true
+            // packings.forEach(packing => {
+            //     runSM(setting, packing, controlPoints)
+            // })
 
-    //         }, setting.job_schedule_time_in_sec * 1000)
-    //     }
-    // })
-}
+            spinner.succeed("Finished!");
+
+            //open the semmaphor
+            nextSemaphor = true;
+         }, setting.job_schedule_time_in_sec * 1000);
+      }
+   });
+};
 
 // const iteratePackings = (setting, packings, controlPoints) => {
 //     for(let packing of packings) {
@@ -66,12 +59,12 @@ module.exports = async () => {
 // }
 
 const iteratePackings = async (setting, packings, controlPoints) => {
-    for await (let packing of packings) {
-        runSM(setting, packing, controlPoints)
-    }
-}
+   for await (let packing of packings) {
+      runSM(setting, packing, controlPoints);
+   }
+};
 
 const getSettings = async () => {
-    const settings = await Setting.find({})
-    return settings[0]
-}
+   const settings = await Setting.find({});
+   return settings[0];
+};
