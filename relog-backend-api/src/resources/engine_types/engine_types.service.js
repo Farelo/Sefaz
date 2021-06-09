c0cae6a5d79066467cae1d9c66d0b5bdf3f7fec2
Gabriel Santos <gabriel.araujo@sistemafiepe.org.br>
2021-06-09 10:09:46 -0300

    Routing engineTypes on services and model

--- relog-backend-api/src/resources/engine_types/engine_types.service.js
+++ relog-backend-api/src/resources/engine_types/engine_types.service.js
@@ -1,13 +1,13 @@
 const debug = require('debug')('service:engine_types')
 const _ = require('lodash')
-const { Engine_type } = require('./engine_types.model')
+const { EngineType } = require('./engine_types.model')
 const { Route } = require('../routes/routes.model')
 
 exports.get_engine_types = async (code) => {
     try {
         if (!code) return await EngineType.find()
 
-        const data = await EngineType.findByName(code);
+        const data = await EngineType.findByName(code)
         return data ? [data] : []
     } catch (error) {
         throw new Error(error);
@@ -18,47 +18,47 @@ exports.get_engine_types = async (id) => {
     try {
         const engine_type = await EngineType
             .findById(id)
-        return engine_type;
+        return engine_type
     } catch (error) {
-        throw new Error(error);
+        throw new Error(error)
     }
 }
 
 exports.find_by_code = async (code) => {
     try {
-        const engine_type = await EngineType.findByCode(code);
-        return engine_type;
+        const engine_type = await EngineType.findByCode(code)
+        return engine_type
     } catch (error) {
-        throw new Error(error);
+        throw new Error(error)
     }
 }
 
 exports.create_engine_type = async (engine_type) => {
     try {
-        const new_engine_type = new EngineType(engine_type);
-        await new_engine_type.save();
-        return new_engine_type;
+        const new_engine_type = new EngineType(engine_type)
+        await new_engine_type.save()
+        return new_engine_type
     } catch (error) {
-        throw new Error(error);
+        throw new Error(error)
     }
 }
 
 exports.find_by_id = async (id) => {
     try {
         const engine_type = await EngineType.findById(id);
-        return engine_type;
+        return engine_type
     } catch (error) {
-        throw new Error(error);
+        throw new Error(error)
     }
 }
 
 exports.update_engine_type = async (id, engine_type_edited) => {
     try {
-        const options = { runValidators: true, new: true };
-        const engine_type = await EngineType.findByIdAndUpdate(id, engine_type_edited, options);
+        const options = { runValidators: true, new: true }
+        const engine_type = await EngineType.findByIdAndUpdate(id, engine_type_edited, options)
 
-        return engine_type;
+        return engine_type
     } catch (error) {
-        throw new Error(error);
+        throw new Error(error)
     }
 }
